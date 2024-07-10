76605f74afab20a0246bf115c2bbf8cd62d206a2
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-07-10 20:16:05 +0530

    yeah

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import { SearchBar } from "@tarzi/jarvis"
 import { ThemeProvider } from "./components/theme-provider"
-import { Switch } from "./components/ui/switch"
-
-const Header = () => {
-  return (
-    <header className="text-center p-8">
-      <Switch />
-    </header>
-  )
-}
-
-const Footer = () => {
-  return <footer>footer</footer>
-}
+import { ThemeToggle } from "./components/mode-toggle"
+import { SearchBar } from "@tarzi/jarvis"
+import { WobbleCard } from "./components/ui/wobble-card"
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="h-screen w-screen grid grid-rows-[auto_1fr_auto]">
-        <Header />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="bg-white dark:bg-black flex justify-center">
+        <div className="h-screen w-full">
+          <div className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex-col pt-6 justify-center">
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]"></div>
 
-        <div className="flex justify-center items-center">
-          <SearchBar />
+            <div className="flex justify-center items-center gap-8 py-12 mb-8">
+              <h1 className="font-LilitaOne text-4xl sm:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-red-200 to-red-500">
+                Jarvis
+              </h1>
+              <ThemeToggle />
+            </div>
+            <div className="flex justify-center">
+              <SearchBar />
+            </div>
+          </div>
         </div>
-
-        <Footer />
       </div>
-    </ThemeProvider>
+    </ThemeProvider >
   )
 }
 
